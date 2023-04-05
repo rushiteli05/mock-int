ecc87a888c84a8a049b1a7a2b2aadfb1852cf3d6
rushikesh <rushikeshteli050@gmail.com>
2023-04-05 11:42:40 +0530

    new add

---
+++ mock.js
@@ -0,0 +1,78 @@
+
+// const input1 = "hello world it is a beautiful day";
+// const str =[]
+// // const work  = input.filter((ele, index)=>{
+// //     input.slice
+// // })
+// for(let i=0; i<=input1.length; i++){
+//     let count = 0;
+//     for(let j=0;j<=input1.length; j++){
+//         if(str[i] ===str[j]){
+//             count ++
+//         }
+//         str[input1[i]]= count
+//     }
+
+// }
+// console.log(str)
+
+// const arr = [10,20,30,10,40,50,20];
+// const obj = []
+
+// for(let i=0;i<=arr.length; i++){
+//     for(let j=0;j<=arr.length; j++){
+//         if(arr[i]=arr[j]){
+//             duplicate.push();
+//         }
+//         obj[arr[i]]
+//     }
+// }
+// console.log(obj)
+
+// const input =[1,2,3,4,5,6];
+// const newlist  =input.map((a,b)=>{
+//     return a*b;
+
+// })
+// console.log(newlist);
+
+// const str = "hello world";
+// const obj= '';
+// for(let i =0;i<=str.length; i++){
+//     for(let j=0; j<=str.length; j++){
+//     if(str[i]===str[j]){
+//         str.push();
+//     }
+//     obj[str[i]]
+// }
+// }
+// console.log(obj)
+
+// const input = 'abc'
+// const newlist  = input.concat("***");
+// console.log(newlist)
+
+// const list = [1,2,3,4];
+// const newlist  = list.map((value)=>value+4);
+// console.log(newlist)
+
+// const input = ['jane:1','doe:2'];
+
+// const input= [
+//     {id:1,name:"keten",age:23},
+//     {id:2,name:"sam",age:24},
+//     {id:3,name:"raj",age:25},
+
+// ]
+
+// const age = input.filter((val)=>{
+//     return val.age === age;
+// })
+// console.log(ages)
+
+
+
+
+
+
+
